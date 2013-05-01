3a1195612dbc971df7c71630cef69e040ac7386d
jitu68 <jitu.pathak62@gmail.com>
2013-05-01 13:07:58 +0530

    rollback

--- includes/search.php
+++
@@ -1,4 +0,0 @@
-<form method="post" class="searchform" action="#">
-			<p><input type="text" name="search_query" class="textbox" />
-  			<input type="submit" name="search" class="button" value="Search" /></p>
-		</form>
